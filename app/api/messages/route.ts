6bedad1f8a88b9c8d6fd0e246aea41e7721d3a4e
ha4219 <ha4219@naver.com>
2023-05-14 01:34:44 +0900

    messages creation, message image upload

---
+++ app/api/messages/route.ts
@@ -0,0 +1,66 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentuser = await getCurrentUser();
+    const body = await request.json();
+    const { message, image, conversationId } = body;
+    if (!currentuser?.id || !currentuser.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const newMessage = await prisma.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: currentuser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: currentuser.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (err: any) {
+    console.log(err, "err_msg");
+    return new NextResponse("InternalError", { status: 500 });
+  }
+}
